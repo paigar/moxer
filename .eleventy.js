06bc362b4909d504a7a05745df13a8c9c60206f0
Juanjo <juanjo@paigar.es>
2026-02-22 12:19:34 +0100

    contar distribuidores

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,16 @@ module.exports = function(eleventyConfig) {
   // Filters
   eleventyConfig.addFilter("slice", (arr, start, end) => arr.slice(start, end));
   eleventyConfig.addFilter("countUniqueBy", (arr, key) => new Set(arr.map(i => i[key])).size);
+  eleventyConfig.addFilter("groupBy", (arr, key) => {
+    const groups = {};
+    arr.forEach(item => {
+      const val = item[key];
+      if (!groups[val]) groups[val] = [];
+      groups[val].push(item);
+    });
+    return Object.entries(groups).map(([name, items]) => ({ name, items }));
+  });
+  eleventyConfig.addFilter("exclude", (arr, key, value) => arr.filter(i => i[key] !== value));
 
   return {
     dir: {
